e896ea86e7a6444d7251896f057879f6dab413b1
Takashi MAEDA <t.maeda1985@gmail.com>
2022-03-13 15:06:05 +0900

    Add task to the list

--- LazyManager/ContentView.swift
+++ LazyManager/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var taskName: String = ""
+    @State var tasks = [String]()
+    
     var body: some View {
-        Text("Hello, world!")
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Enter new task", text: $taskName)
+                        .accessibilityIdentifier("taskNameTextField")
+                    
+                    Button("Create") {
+                        self.tasks.append(self.taskName)
+                    }
+                    .accessibilityIdentifier("addTaskButton")
+                }
+                List(self.tasks, id: \.self) { name in
+                    Text("\(name)")
+                }
+                Spacer()
+            }
             .padding()
+            .navigationBarTitle("Tasks")
+        }
     }
 }
 
--- LazyManagerUITests/LazyManagerUITests.swift
+++ LazyManagerUITests/LazyManagerUITests.swift
@@ -7,36 +7,27 @@
 
 import XCTest
 
-class LazyManagerUITests: XCTestCase {
+class when_user_types_task_name_and_press_add_button: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    private var app: XCUIApplication!
+    
+    override func setUp() {
+        self.app = XCUIApplication()
+        self.app.launch()
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    
+    func test_should_add_task_to_the_list() {
+        let taskNameField = self.app.textFields["taskNameTextField"]
+        taskNameField.tap()
+        taskNameField.typeText("Wash the car \n")
+        let addTaskButton = self.app.buttons["addTaskButton"]
+        addTaskButton.tap()
+        let taskCount = self.app.tables.children(matching: .cell).count
+        XCTAssertEqual(1, taskCount)
     }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    
+    // test_should_clear_task_name_field
+    // test_should_not_add_task_when_task_name_is_blank
+    
+    
 }
