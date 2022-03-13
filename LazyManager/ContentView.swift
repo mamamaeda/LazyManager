11185ac5fd2e302b29953c29e90d8278a50188d1
Takashi MAEDA <t.maeda1985@gmail.com>
2022-03-13 14:17:45 +0900

    XCode project

---
+++ LazyManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LazyManager
+//
+//  Created by Takashi MAEDA on 2022/03/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LazyManager/LazyManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyManagerApp.swift
+//  LazyManager
+//
+//  Created by Takashi MAEDA on 2022/03/13.
+//
+
+import SwiftUI
+
+@main
+struct LazyManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
