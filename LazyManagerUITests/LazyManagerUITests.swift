6e2d430abfd322b866d30ef537ae53c466513dbe
Takashi MAEDA <t.maeda1985@gmail.com>
2022-03-13 15:40:10 +0900

    Refactoring Test

--- LazyManagerUITests/LazyManagerUITests.swift
+++ LazyManagerUITests/LazyManagerUITests.swift
@@ -16,28 +16,36 @@ class when_user_types_task_name_and_press_add_button: XCTestCase {
         self.app.launch()
     }
     
-    func test_should_add_task_to_the_list() {
-        let taskNameField = self.app.textFields["taskNameTextField"]
+    private func taskNameTextField() -> XCUIElement{
+        self.app.textFields["taskNameTextField"]
+    }
+    
+    private func typeTextToTaskNameTextField(_ text: String) {
+        let taskNameField = taskNameTextField()
         taskNameField.tap()
-        taskNameField.typeText("Wash the car \n")
+        taskNameField.typeText(text)
+    }
+    
+    private func tapAddTaskButton() {
         let addTaskButton = self.app.buttons["addTaskButton"]
         addTaskButton.tap()
+    }
+    
+    func test_should_add_task_to_the_list() {
+        typeTextToTaskNameTextField("Wash the car \n")
+        tapAddTaskButton()
         let taskCount = self.app.tables.children(matching: .cell).count
         XCTAssertEqual(1, taskCount)
     }
     
     func test_should_clear_task_name_field() {
-        let taskNameField = self.app.textFields["taskNameTextField"]
-        taskNameField.tap()
-        taskNameField.typeText("Wash the car \n")
-        let addTaskButton = self.app.buttons["addTaskButton"]
-        addTaskButton.tap()
-        XCTAssertEqual("Enter new task", taskNameField.value as! String)
+        typeTextToTaskNameTextField("Wash the car \n")
+        tapAddTaskButton()
+        XCTAssertEqual("Enter new task", taskNameTextField().value as! String)
     }
     
     func test_should_not_add_task_when_task_name_is_blank() {
-        let addTaskButton = self.app.buttons["addTaskButton"]
-        addTaskButton.tap()
+        tapAddTaskButton()
         let taskCount = self.app.tables.children(matching: .cell).count
         XCTAssertEqual(0, taskCount)
     }
